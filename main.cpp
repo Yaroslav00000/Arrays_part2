8522cf025b16071d591805d9b86a89b870d0dbfb
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-09 20:32:26 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
-	
+	int arr[] = { 1, 2, 0, 9, 0, 3, 0, 5 };
+	int n = 8;
+	int j = 0;
+	for (int i = 0; i < n; ++i) {
+		if (arr[i] != 0) {
+			arr[j++] = arr[i];
+		}
+	}
+	while (j < n) {
+		arr[j++] = -1;
+	}
+	for (int i = 0; i < n; ++i) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
