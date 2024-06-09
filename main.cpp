29f85e00d96036dd6296801780ad4b91b7be0a5e
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-06-09 22:18:30 +0300

    Task complited

--- main.cpp
+++ main.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main()
 {
-	
+	int arr[] = { 5,-8, 10, -15, 1, 0, -6, 4, -3, 10};
+	int arr_1[5];
+	int arr_2[5];
+	for (int i = 0; i < 5; i++)
+	{
+		arr_1[i] = arr[i];
+		arr_2[i] = arr[i + 5];
+	}
+	cout << "Array 1: ";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr_1[i] << " ";
+	}
+	cout << endl;
+	cout << "Array 2: ";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr_2[i] << " ";
+	}
+	cout << endl;
 	return 0;
 }
